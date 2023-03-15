5eef1fed9e366561ec7f25746ae27d54066cf39e
Djr719 <druggiero197@gmail.com>
2023-03-15 11:25:50 -0400

    Trying to write JavaScript code

--- Develop/script.js
+++ Develop/script.js
@@ -7,65 +7,15 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ]; 
+var length = (x>==8)+(x<==128)
+var x = 8
 
-
-function questions() {
-  var isValid = false;
-  do {
-    var length = prompt(Choose password length between 8 and 128 characters);
-    var responses = {
-      length: '8-128',  
-      special:special, 
-      numbers:numbers, 
-      upperCase:upperCase, 
-      lowerCase:lowerCase
-    } 
-    if((length < 8)||(length > 128))
-    alert("Choose number between 8 and 128");
-    else
-    isValid = true;
-
-  } while(!isValid);
-  return responses;
+numberOfCharacters = prompt("Please enter your password length. Password must be between 8 and 128 characters.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    return "Error. Password must be between 8 and 128 characters";
+  } else if (NaN(Error);(numberOfCharacters)) {
+  
 }
-// This function joins all the user responses and then creates the result - a strong password.
-function generatePassword() {
-  var passwordOptions = upperCase; lowerCase; numbers; special
-  var possibleCombo = [];
-  var finalPassword = "";
-
-
-
-  if (passwordOptions.Numbers) {
-    for (var i of numbers)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.LowerCase) {
-    for (var i of lowerCase)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.UpperCase) {
-    for (var i of upperCase)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.Special) {
-    for (var i of special)
-      possibleCombo.push(i);
-  }
-
-
-  console.log(possibleCombo);
-
-
-  for (var i = 0; i < passwordOptions.length; i++) {
-    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
-    
-  }
-  console.log(Password);
-
-  return Password;
-}
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
